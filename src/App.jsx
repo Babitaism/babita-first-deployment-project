40f67bfe12b9ea9a0a1246ce4949b44696ba6143
babitaism <rbabita044@gmail.com>
2026-04-17 14:13:11 +0530

    aaded react app

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import ganpati from "./assets/ganpati.jpg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [value, setValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:5000/api/users", value);
+      console.log(res, "Response:", res.data);
+    } catch (err) {
+      console.log("Error:", err);
+    }
+    if (!value.email || !value.password) {
+      alert("Please fill all fields");
+      return;
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value: inputValue } = e.target;
+    setValue((prev) => ({
+      ...prev,
+      [name]: inputValue,
+    }));
+  };
+
+  return (
+    <>
+      <div className="main-box">
+        <div className="left-side">
+          <img src={ganpati} alt="banner-image" />
+        </div>
+        <div className="right-side">
+          <div className="input-box">
+            <h3>Login to your account</h3>
+            <input
+              className="input"
+              type="text"
+              onChange={handleChange}
+              name="email"
+              value={value.email}
+              placeholder="   Enter your email"
+            />
+            <input
+              className="input-pwd"
+              onChange={handleChange}
+              name="password"
+              type="password"
+              value={value.password}
+            />
+            <button className="primary-btn" onClick={handleLogin}>
+              Log in
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
